13bfee771cadce371e6f43ef3b8b3ef417a34cdd
Doiinn <s7070103@kmitl.ac.th>
2014-12-04 02:20:50 +0700

    initial main window

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+"""
+==== GUI-Pylint ====
+Doiinn : 57070103
+Madooding : 57070112
+====================
+"""
 from Tkinter import *
-
 root = Tk()
-
-w = Label(root, text="Hello, world!")
-w.pack()
+root.title("GUI-Pylint")    #title bar of app
+frame = Frame(width=450, height=450) #frame(or windows) size
+frame.pack()
 
 root.mainloop()
